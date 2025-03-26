608d24b967c8609c015f54bf4a336509eb83b7cc
yeliena-bochagova <lenabochagov.a27@gmail.com>
2025-03-26 17:34:01 +0200

    Додано функцію filter_last_month_data

--- main.py
+++ main.py
@@ -18,3 +18,8 @@ def read_data_from_file(filename):
                 continue  # Пропускаємо рядки з помилками у форматі
     return data
 
+def filter_last_month_data(data):
+    """Фільтрує записи за останній місяць."""
+    today = datetime.date.today()
+    last_month = today - datetime.timedelta(days=30)
+    return [entry for entry in data if entry[1] >= last_month]
